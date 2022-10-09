4adff00386eb6163602351226b7d25213d056632
Cimorexave <sadeq.s502@gmail.com>
2022-10-09 13:17:12 +0330

    feat: initialized new rust package using cargo

---
+++ rust-clc/target/debug/deps/rust_clc-b814f1ac5e35c08d.d
@@ -0,0 +1,5 @@
+c:\Users\armit\OneDrive\Desktop\rust-sandbox\rust-clc\target\debug\deps\rust_clc-b814f1ac5e35c08d.rmeta: src\main.rs
+
+c:\Users\armit\OneDrive\Desktop\rust-sandbox\rust-clc\target\debug\deps\rust_clc-b814f1ac5e35c08d.d: src\main.rs
+
+src\main.rs:

886f36e87c71f10607c6dbf44ec67ecb94618449
Cimorexave <sadeq.s502@gmail.com>
2022-10-09 19:17:55 +0330

    feat: string formatting

--- rust-clc/src/main.rs
+++ rust-clc/src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+    //outputting
+    // println!("Hello, world!");
+
+    println!(
+        "this is the {0} index and {1} is the next {index} {2}. You could do a {lot}",
+        0,
+        1,
+        2,
+        index = "index",
+        lot = "lot of things with this string formatting capablitiy of rust..."
+    );
 }

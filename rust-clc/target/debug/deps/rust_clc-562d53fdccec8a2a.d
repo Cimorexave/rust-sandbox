c57d93b924e9af170ea36023e3826dada5e52977
Cimorexave <sadeq.s502@gmail.com>
2022-10-09 13:24:02 +0330

    removed target files

--- rust-clc/target/debug/deps/rust_clc-562d53fdccec8a2a.d
+++
@@ -1,5 +0,0 @@
-c:\Users\armit\OneDrive\Desktop\rust-sandbox\rust-clc\target\debug\deps\rust_clc-562d53fdccec8a2a.rmeta: src\main.rs
-
-c:\Users\armit\OneDrive\Desktop\rust-sandbox\rust-clc\target\debug\deps\rust_clc-562d53fdccec8a2a.d: src\main.rs
-
-src\main.rs:
